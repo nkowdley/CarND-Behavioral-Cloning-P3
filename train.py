aa99e578b26bfdc62413e384a63e656a9c4caa56
Neel Kowdley <nkowdley@gmail.com>
2018-07-13 21:55:46 -0400

    fix typo

--- train.py
+++ train.py
@@ -10,7 +10,7 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 from keras.models import Model, Sequential
 from keras.layers import Flatten, Dense, Lambda
-from keras.layers.convolutional.Cropping2D
+from keras.layers.convolutional import Cropping2D
 import matplotlib.pyplot as plt
 
 # Globals for Training/Testing
