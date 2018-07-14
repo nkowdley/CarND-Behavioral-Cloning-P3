90917148186f4c4d74f2b3b5ec6b9162a337b19b
Neel Kowdley <nkowdley@gmail.com>
2018-07-13 22:25:01 -0400

    try something new

--- train.py
+++ train.py
@@ -91,13 +91,13 @@ model.add(Dense(1))
 model.compile(loss='mse', optimizer='adam')
 
 # train the model
-model.fit(X_train, y_train, validation_split=.2, shuffle=True, nb_epoch=EPOCHS)
+#model.fit(X_train, y_train, validation_split=.2, shuffle=True, nb_epoch=EPOCHS)
 
-#history_object = model.fit_generator(train_generator, samples_per_epoch =
-#    len(train_samples), validation_data = 
-#    validation_generator,
-#    nb_val_samples = len(validation_samples), 
-#    nb_epoch=EPOCHS, verbose=1)
+history_object = model.fit_generator(train_generator, samples_per_epoch =
+    len(train_samples), validation_data = 
+    validation_generator,
+    nb_val_samples = len(validation_samples), 
+    nb_epoch=EPOCHS, verbose=1)
 
 # print the keys contained in the history object
 #print(history_object.history.keys())
