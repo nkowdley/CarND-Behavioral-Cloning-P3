688d246a5777c5be99846c15302aee72d83c0bd1
Neel Kowdley <nkowdley@gmail.com>
2018-07-15 14:17:42 -0400

    fix model again

--- train.py
+++ train.py
@@ -77,7 +77,7 @@ model = Sequential()
 #Normalize the data
 model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=(160, 320, 3))) #normalize the data and give it a mean of 0
 # Crop the data
-model.add(Cropping2D(cropping=((50,20),(0,0))))
+model.add(Cropping2D(cropping=((70,25),(0,0))))
 # Nvidia model taken from: https://devblogs.nvidia.com/deep-learning-self-driving-cars/
 model.add(Convolution2D(24, 5, 5, subsample=(2,2), activation='relu'))
 model.add(Convolution2D(36, 5, 5, subsample=(2,2), activation='relu'))
@@ -85,12 +85,13 @@ model.add(Convolution2D(48, 5, 5, subsample=(2,2), activation='relu'))
 model.add(Convolution2D(64, 3, 3, activation='relu'))
 model.add(Convolution2D(64, 3, 3, activation='relu'))
 model.add(Flatten())
-model.add(Dense(100)
-model.add(Dense(50)
-model.add(Dense(10)
-model.add(Dense(1))
+model.add(Dense(1164))
+model.add(Dense(100))
+model.add(Dense(50))
+model.add(Dense(10))
+model.add(Dense(1)))
 # compile the model
-model.compile(loss='mse', optimizer='adam')
+model.compile(loss='mae', optimizer='adam')
 
 # train the model
 #model.fit(X_train, y_train, validation_split=.2, shuffle=True, nb_epoch=EPOCHS)
