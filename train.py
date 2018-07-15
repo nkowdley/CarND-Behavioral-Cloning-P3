999abb850bcccd79138d3409f23ba5e74c843898
Neel Kowdley <nkowdley@gmail.com>
2018-07-15 12:15:15 -0400

    another try

--- train.py
+++ train.py
@@ -79,11 +79,11 @@ model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=(160, 320, 3))) #normali
 model.add(Cropping2D(cropping=((70,25),(0,0))))
 # Nvidia model taken from: https://devblogs.nvidia.com/deep-learning-self-driving-cars/
 model.add(Convolution2D(24, 5, 5, border_mode='same', subsample=(2,2), activation='relu'))
-#model.add(MaxPooling2D(pool_size=(2, 2), strides=(1, 1)))
+model.add(MaxPooling2D(pool_size=(2, 2), strides=(1, 1)))
 model.add(Convolution2D(36, 5, 5, border_mode='same', subsample=(2,2), activation='relu'))
-#model.add(MaxPooling2D(pool_size=(2, 2), strides=(1, 1)))
+model.add(MaxPooling2D(pool_size=(2, 2), strides=(1, 1)))
 model.add(Convolution2D(48, 5, 5,border_mode='same', subsample=(2,2), activation='relu'))
-#model.add(MaxPooling2D(pool_size=(2, 2), strides=(1, 1)))
+model.add(MaxPooling2D(pool_size=(2, 2), strides=(1, 1)))
 model.add(Convolution2D(64, 3, 3, border_mode='same', activation='relu'))
 model.add(Convolution2D(64, 3, 3, activation='relu'))
 model.add(Flatten())
@@ -93,7 +93,7 @@ model.add(Dense(50))
 model.add(Dense(10))
 model.add(Dense(1))
 # compile the model
-model.compile(loss='mse', optimizer='adam')
+model.compile(loss='mae', optimizer='adam')
 
 # train the model
 #model.fit(X_train, y_train, validation_split=.2, shuffle=True, nb_epoch=EPOCHS)
