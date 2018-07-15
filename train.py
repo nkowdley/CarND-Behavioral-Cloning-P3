e08945776acae2ae583dc5ce8da9da8108d306f7
Neel Kowdley <nkowdley@gmail.com>
2018-07-15 17:12:35 -0400

    try to resize the image

--- train.py
+++ train.py
@@ -116,7 +116,8 @@ validation_generator = generator(validation_samples, batch_size = BATCH_SIZE)
 #Instantiate the model
 model = Sequential()
 #Normalize the data
-model.add(Lambda(lambda x:  x / 127.5 - 1. , input_shape=(160, 320, 3))) #normalize the data and give it a mean of 0
+model.add(Lambda(lambda x: K.tf.image.resize_images(x, (80, 160)), input_shape=(160, 320, 3)))
+model.add(Lambda(lambda x:  x / 127.5 - 1.)) #normalize the data and give it a mean of 0
 # Crop the data
 model.add(Cropping2D(cropping=((70,25),(0,0))))
 # Nvidia model taken from: https://devblogs.nvidia.com/deep-learning-self-driving-cars/
