8418dc23c86d9ceb499f65a2344fcd41bf4cb87a
Neel Kowdley <nkowdley@gmail.com>
2018-10-27 11:51:40 -0400

    fix cropping call

--- train2.py
+++ train2.py
@@ -109,7 +109,7 @@ Y_train = np.array(augmented_measurements)
 #Instantiate the model
 model = Sequential()
 model.add(Lambda(lambda x: x / 255.0 - .5, input_shape=(160,320,3)))
-model.add(Cropping2D(cropping=(70,25),(0,0)))
+model.add(Cropping2D(cropping=((70,25),(0,0)))
 model.add(Convolution2D(6,5,5, activation= "relu"))
 model.add(MaxPooling2D())
 model.add(Convolution2D(16,5,5, activation= "relu"))
